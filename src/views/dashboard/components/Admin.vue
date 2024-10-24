82aa787659c15fdc0632742f8b7a4a8e6cd7483e
whutzn <whutzn@foxmail.com>
2024-10-24 16:16:22 +0800

    feat: add map 2d&3d view

--- src/views/dashboard/components/Admin.vue
+++ src/views/dashboard/components/Admin.vue
@@ -1,14 +1,164 @@
 <template>
   <div class="app-container center">
-    <el-empty description="欢迎来到 admin 角色专属首页" />
+    <div id="sceneViewer" />
   </div>
 </template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue"
+import {
+  Ion,
+  Viewer,
+  Cesium3DTileset,
+  CesiumTerrainProvider,
+  WebMapTileServiceImageryProvider,
+  GeographicTilingScheme,
+  ShadowMode
+} from "cesium"
+import "cesium/Build/Cesium/Widgets/widgets.css"
+import { setTilesHeight } from "@/utils/map/map3d"
+
+onMounted(async () => {
+  Ion.defaultAccessToken =
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiIyZWI3MjgzNy1mN2RiLTQ3ODUtYTYyMy00YWQ2ZGJiYjllMTAiLCJpZCI6MTQ4MTgyLCJpYXQiOjE2ODczMTIyMDl9.XpOkgvofy7JCkrtEkMyQ7wsEVJuYQSaATGEAtepxH4Q"
+  const viewer = new Viewer("sceneViewer", {
+    animation: false, //动画小组件
+    baseLayerPicker: false, //底图选择组件
+    fullscreenButton: false, //全屏组件
+    vrButton: false,
+    geocoder: false,
+    homeButton: false,
+    infoBox: false,
+    scene3DOnly: true,
+    sceneModePicker: false,
+    selectionIndicator: false,
+    timeline: false,
+    navigationHelpButton: false,
+    navigationInstructionsInitiallyVisible: false,
+    baseLayer: false,
+    msaaSamples: 4
+  })
+  // 水雾特效
+  viewer.scene.globe.showGroundAtmosphere = true
+
+  /**
+   * 加载火星地形
+   */
+  const MARS_TERRIAN = await CesiumTerrainProvider.fromUrl("http://data.mars3d.cn/terrain")
+  viewer.terrainProvider = MARS_TERRIAN
+
+  const TDT_IMG = new WebMapTileServiceImageryProvider({
+    url: "http://t0.tianditu.gov.cn/img_w/wmts?tk=63daddeb93bedd7502558f4170212453",
+    layer: "img",
+    style: "default",
+    tileMatrixSetID: "w",
+    format: "tiles",
+    maximumLevel: 18
+  })
+  const TDT_CIA = new WebMapTileServiceImageryProvider({
+    url: "http://t0.tianditu.gov.cn/cia_w/wmts?tk=63daddeb93bedd7502558f4170212453",
+    layer: "cia",
+    style: "default",
+    tileMatrixSetID: "w",
+    format: "tiles",
+    maximumLevel: 18
+  })
+
+  viewer.imageryLayers.addImageryProvider(TDT_IMG)
+  viewer.imageryLayers.addImageryProvider(TDT_CIA)
+
+  if (process.env.NODE_ENV === "development") viewer.scene.debugShowFramesPerSecond = true
+  // viewer.camera.flyTo({
+  //   destination: Cartesian3.fromDegrees(121.45, 31.22, 600),
+  //   orientation: {
+  //     heading: CesiumMath.toRadians(0.0),
+  //     pitch: CesiumMath.toRadians(-15.0)
+  //   }
+  // })
+  /**
+   * 加载谷歌三维实景，中国区域基本不可用
+   **/
+  // viewer.scene.primitives.add(await Cesium3DTileset.fromIonAssetId(2275207))
+  // const buildingTileset = await createOsmBuildingsAsync()
+  // viewer.scene.primitives.add(buildingTileset)
+
+  const TANG_XIA = await Cesium3DTileset.fromUrl("http://192.168.0.35:9003/model/tjOpRNmKP/tileset.json", {
+    shadows: ShadowMode.DISABLED,
+    dynamicScreenSpaceError: true,
+    dynamicScreenSpaceErrorDensity: 2.0e-4,
+    dynamicScreenSpaceErrorFactor: 24.0,
+    dynamicScreenSpaceErrorHeightFalloff: 0.25,
+    maximumScreenSpaceError: 7.5,
+    cacheBytes: 1073741824,
+    maximumCacheOverflowBytes: 2147483648,
+    skipLevelOfDetail: true,
+    preferLeaves: true
+  })
+  viewer.scene.primitives.add(TANG_XIA)
+  viewer.scene.globe.depthTestAgainstTerrain = true
+  viewer.zoomTo(TANG_XIA)
+  setTilesHeight(TANG_XIA, 8)
+
+  const _matrixIds = [
+    "EPSG:4326:0",
+    "EPSG:4326:1",
+    "EPSG:4326:2",
+    "EPSG:4326:3",
+    "EPSG:4326:4",
+    "EPSG:4326:5",
+    "EPSG:4326:6",
+    "EPSG:4326:7",
+    "EPSG:4326:8",
+    "EPSG:4326:9",
+    "EPSG:4326:10",
+    "EPSG:4326:11",
+    "EPSG:4326:12",
+    "EPSG:4326:13",
+    "EPSG:4326:14",
+    "EPSG:4326:15",
+    "EPSG:4326:16",
+    "EPSG:4326:17",
+    "EPSG:4326:18",
+    "EPSG:4326:19",
+    "EPSG:4326:20",
+    "EPSG:4326:21"
+  ]
+
+  const ZJD_TILE = new WebMapTileServiceImageryProvider({
+    url: "http://192.168.0.90:8080/geoserver/leqing/gwc/service/wmts",
+    layer: "leqing:tdlygh",
+    style: "",
+    format: "image/png",
+    tileMatrixSetID: "EPSG:4326",
+    tileMatrixLabels: _matrixIds,
+    tilingScheme: new GeographicTilingScheme(),
+    maximumLevel: 20
+  })
+  viewer.imageryLayers.addImageryProvider(ZJD_TILE)
+})
+</script>
 
 <style lang="scss" scoped>
 .center {
   height: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+
+  #sceneViewer {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+}
+</style>
+
+<style>
+.cesium-widget-credits {
+  /* 去除logo */
+  display: none !important;
 }
 </style>
--- src/views/dashboard/components/Editor.vue
+++ src/views/dashboard/components/Editor.vue
@@ -1,14 +1,97 @@
 <template>
   <div class="app-container center">
-    <el-empty description="欢迎来到 editor 角色专属首页" />
+    <div id="map" />
   </div>
 </template>
+
+<script lang="ts" setup>
+import { onMounted } from "vue"
+import Map from "ol/Map"
+import TileLayer from "ol/layer/Tile"
+import View from "ol/View"
+import "ol/ol.css"
+import { TileWMS } from "ol/source"
+import VectorLayer from "ol/layer/Vector"
+import VectorSource from "ol/source/Vector"
+import GeoJson from "ol/format/GeoJSON"
+import { tile } from "ol/loadingstrategy"
+import { createXYZ } from "ol/tilegrid"
+import { Fill, Stroke, Style, Text } from "ol/style"
+
+onMounted(() => {
+  const layers = [
+    new TileLayer({
+      source: new TileWMS({
+        url: "http://192.168.0.90:8080/geoserver/zjd/wms",
+        params: { FORMAT: "image/png", VERSION: "1.1.1", tiled: true, STYLES: "", layers: "zjd:zjdzd_copy2" }
+      }),
+      maxZoom: 16
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJson(),
+        url: (extent) => {
+          return (
+            "http://192.168.0.90:8080/geoserver/zjd/wms?service=WFS&version=1.1.0&request=GetFeature&typename=zjd:zjdzd_copy2&outputFormat=application/json&srsname=EPSG:4326&bbox=" +
+            extent.join(",") +
+            ",EPSG:4326"
+          )
+        },
+        strategy: tile(createXYZ({ tileSize: 512 }))
+      }),
+      minZoom: 16,
+      style: (feature) =>
+        new Style({
+          stroke: new Stroke({
+            color: "#ff0000",
+            width: 1
+          }),
+          fill: new Fill({
+            color: "rgba(0,0,0,0)"
+          }),
+          text: new Text({
+            text: feature.get("zjddm"),
+            font: "14px sim-sun",
+            fill: new Fill({
+              color: "rgba(255, 255, 255, 1)"
+            }),
+            stroke: new Stroke({
+              color: "#1b1c1d",
+              width: 2
+            })
+          })
+        })
+    })
+  ]
+  const map = new Map({
+    layers: layers,
+    target: "map",
+    view: new View({
+      center: [125.26323990075572, 35.885796337603196],
+      projection: "EPSG:4326",
+      zoom: 6
+    })
+  })
+
+  map.on("moveend", (e) => {
+    console.log(e.target.getView().getZoom(), e.target.getView().getCenter())
+  })
+})
+</script>
 
 <style lang="scss" scoped>
 .center {
   height: 100%;
+  width: 100%;
   display: flex;
   justify-content: center;
   align-items: center;
+  #map {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
 }
 </style>
